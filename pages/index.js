aa175fdc50e159c0c16ac41e02d1a466df0e9990
Eeeeeeeeeeeeen <ianthomson92@hotmail.co.uk>
2021-07-27 13:00:51 +0100

    Added social media placeholders & darmode icons

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import { Box, Button, Center, Flex, Grid, HStack, Spacer, Stack, Text, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Box, Button, Center, Flex, Grid, Spacer, Stack, Text, useColorMode } from "@chakra-ui/react";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import Game from "../components/Game";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
 export default function Home() {
   const [data, setData] = useState()
-  const { toggleColorMode } = useColorMode()
+  const { toggleColorMode, colorMode } = useColorMode()
 
   useEffect(() => {
     fetch("Countries.json")
@@ -23,11 +22,22 @@ export default function Home() {
       </Head>
 
       <Grid h="100vh" templateRows="4rem 1fr">
-        <Box as="header" backgroundColor="teal">
+        <Box as="header">
           <Flex m="0 10px" height="100%" alignItems="center">
-            <Text as="span" textColor="white">Header tbc</Text>
+            <Text as="a" href="/" fontSize="xl" fontWeight="bold">Flag Quiz</Text>
             <Spacer />
-            <Button colorScheme="cyan" onClick={toggleColorMode}>Toggle Dark Mode</Button>
+            {/* <HStack spacing="5px">
+              <Button as="a" colorScheme="teal" href="https://github.com/eeeeeeeeeeeeen/flag-quiz" target="_blank" rel="noopener noreferrer">
+                <GithubIcon boxSize="8" colorMode={colorMode} />
+              </Button>
+              <Button as="a" colorScheme="teal" href="https://twitter.com/eeyan_t" target="_blank" rel="noopener noreferrer">
+                <TwitterIcon boxSize="8" colorMode={colorMode} />
+              </Button>
+            </HStack> */}
+            <Button colorScheme="teal" onClick={toggleColorMode} ml="40px" size="lg">
+              {colorMode !== 'dark' ? <MoonIcon boxSize="8" /> : <SunIcon boxSize="8" />}
+            </Button>
+
           </Flex>
         </Box>
 
